28e2ff5c3c81bc1b21e079a7ba012f7393a9fda5
xzicr <30870274852@qq.com>
2026-03-08 15:15:03 +0800

    Backup 1772954103129

--- 01Balance_chassis_task/application/iwdg_task.c
+++ 01Balance_chassis_task/application/iwdg_task.c
@@ -1,12 +1,15 @@
 #include "iwdg_task.h"
 #include "cmsis_os.h"
 #include "chassis_task.h"
+#include "CAN_receive.h"
 void iwdg_task(void const *pvParamter)
 {
     while(1)
     {
         if(chassis_move.chassis_data_->reset_flag)
         {
+            uint32_t tick;
+            tick = xTaskGetTickCount();
             buzzer_on(50,8399);
             osDelay(100);
             buzzer_off();
@@ -15,7 +18,20 @@ void iwdg_task(void const *pvParamter)
             buzzer_off();        
             buzzer_on(50,8399);
             osDelay(100);
-            buzzer_off();             
+            buzzer_off();  
+            while((xTaskGetTickCount() - tick) < pdMS_TO_TICKS(1000))
+            {
+                if(chassis_move.chassis_posture_info.pitch_angle>0)
+                {
+                    CAN_LK_Boradcast_Control(400,-400,0,0);
+                }
+                else
+                {
+                    CAN_LK_Boradcast_Control(-400,400,0,0);
+                } 
+            }
+             CAN_LK_Boradcast_Control(0,0,0,0);
+             osDelay(100);
             __disable_irq();              // 关闭全局中断
             HAL_NVIC_SystemReset();       // 执行系统复位        
         }
