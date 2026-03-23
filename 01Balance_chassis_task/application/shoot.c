c9fb512408068cb948d6f113e4b379c0131dbb25
xzicr <30870274852@qq.com>
2026-03-23 20:15:20 +0800

    Backup 1774268120806

--- 01Balance_chassis_task/application/shoot.c
+++ 01Balance_chassis_task/application/shoot.c
@@ -18,41 +18,46 @@ uint16_t shoot_state;
 uint8_t process_state = 0;
 int8_t shoot_mode;
 
-void ballet_feq_calc(uint8_t fequence)
+fp32 ShootTime = 0.0f;
+fp32 shoot_time = 0.0f;
+fp32 shoot_speed = 0.0f;
+
+extern robot_status_t robot_state;
+extern power_heat_data_t power_heat_data;
+
+void shoot_speed_calc(void)
 {
-	shoot_control.speed_set = (fequence/8)*60*36;
+	float a = (float)(robot_state.shooter_barrel_cooling_value);
+	float m = (float)(robot_state.shooter_barrel_heat_limit - power_heat_data.shooter_17mm_1_barrel_heat);
+	float d = 10.0f;                
+	if(shoot_time == 0){
+		/*方案二：根据热量上限和冷却决定射击策略，计算得当射击时间为m（热量上限）+1*a（冷却速率）时基本可以抹除冷却优先和爆发优的差距，即两者各级对应射速相近
+				当k增大时，差距射击频率差距主要体现在低等级（爆发高，冷却低），等级越高影响越小。爆发模式下各等级射频更加均匀且持续时间更长，
+				冷却模式正好相反，低等级射频低，高等级射频高且持续时间短，可灵活选择m+k*a*/
+		ShootTime = (m + 2 * a) * 5;
+		fp32_constrain(ShootTime,0,4000);
+		//分级射速
+		if(m < 100){
+			shoot_speed = (10 * m - a - 10 * d) / (d * (ShootTime / 100.0f)) + a / d;
+		}
+		else{
+			shoot_speed = (10 * m - a - 15 * d) / (d * (ShootTime / 100.0f)) + a / d;
+		}
+	}
+	else if(0 < shoot_time && shoot_time < ShootTime){
+	}
+	else{
+		shoot_speed = (a / d) ;
+	}
+	if(shoot_time < ShootTime){
+		shoot_time++;
+	}
+	fp32_constrain(shoot_speed, 0, 25);  // 根据实际电机能力限制
 }
 
-/*重写的播弹盘电机函数*/
-void push_motor_on(void)
+void ballet_feq_calc(fp32 fequence)
 {
-	shoot_control.speed_set=7000;
-}
-
-void trigger_motor_turn_back(void)
-{
-	/* ---------1 rpm <-> 0.10471975511 rad/s------*/
-	/* ----------2006减速比36??1------------------- */
-	//7000rpm  对应输出20.36217460rad/s
-	if(shoot_control.block_time<BLOCK_TIME)
-		shoot_control.speed_set=7000;
-	else
-	{
-		shoot_control.speed_set=-7000;
-	}
-	if(shoot_control.shoot_motor_measure->speed_rpm<BLOCK_TRIGGER_SPEED&&shoot_control.block_time<BLOCK_TIME)
-	{
-		shoot_control.block_time++;
-		shoot_control.reverse_time=0;
-	}
-	else if(shoot_control.block_time==BLOCK_TIME&&shoot_control.reverse_time<REVERSE_TIME)
-	{
-		shoot_control.reverse_time++;
-	}
-	else
-	{
-		shoot_control.block_time=0;
-	}
+	shoot_control.speed_set = (fequence/8.0f)*60*36;
 }
 uint16_t shoot_single_control(void)
 {
@@ -81,7 +86,6 @@ uint16_t shoot_single_control(void)
     }
     return 0;  // 默认返回??
 }
-
 static void shoot_feedback_update(void)
 {
 	shoot_control.speed = shoot_control.shoot_motor_measure->speed_rpm ;
@@ -89,7 +93,6 @@ static void shoot_feedback_update(void)
 	shoot_control.last_press_l=shoot_control.press_l;
 	shoot_control.press_l=(shoot_control.shoot_control_data->shoot_mode_rc >>4) & 0x01;
 	shoot_mode = shoot_control.shoot_control_data->shoot_mode_rc&0x0F;
-	// get_power_shooter_output(&shoot_control.shooter_output);
 }
 void shoot_init()
 {
@@ -110,7 +113,6 @@ void shoot_init()
 	shoot_state = SHOOT_FINISH;
 	
 }
-
 static void shoot_set_mode(void)
 {
 	if(shoot_mode==SHOOT_SINGLE)
@@ -150,16 +152,18 @@ void shoot_control_set()
 	}
 	else if(shoot_state == SHOOT_START_CONTINUE)
 	{
-		ballet_feq_calc(11);
+		shoot_speed_calc();
+		ballet_feq_calc(shoot_speed);
 		single_shoot_cnt = 0;	//单发的这个标志位置0
 
-		/* -------计算PID速度??-------- */
 		PID_calc(&shoot_control.trigger_speed_mode_speed_pid, shoot_control.shoot_motor_measure->speed_rpm, shoot_control.speed_set);
 		shoot_control.given_current=(int16_t)(shoot_control.trigger_speed_mode_speed_pid.out);
 
 		if(shoot_control.shoot_mode!=SHOOT_CONTINUE)
 		{
 			shoot_state = SHOOT_FINISH;
+			shoot_time = 0;
+			shoot_speed = 0;
 		}
 	}
 
